49a00ce4a4cbf3a6145eef32d4c11fd6c57658f1
MohNasrallah <mohamednasrallah14@gmail.com>
2023-07-18 22:05:20 +0300

    commit

--- src/main/java/pages/FirstPage.java
+++ src/main/java/pages/FirstPage.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class FirstPage {
 
-	By Page2Navigator = By.xpath("//*[@id=\"botstuff\"]/div/div[3]/table/tbody/tr/td[3]/a");
 
 	WebDriver driver;
 	WebDriverWait wait;
@@ -18,47 +17,10 @@ public class FirstPage {
 		this.driver=driver; 
 		wait = new WebDriverWait(driver, 10);
 	}
-	By searchResultLocator = new By.ByCssSelector("a h3");
-
-	public List<WebElement> countLinks()
-
-	{
-		driver.findElement(searchResultLocator);
-
-
-		//List<WebElement> elements = driver.findElements(searchResultLocator);
-		//int searchResult = (int) elements.stream().filter(WebElement::isDisplayed).count();
-		//int secondSearchResults = (int) driver.findElements(searchResultLocator).stream().filter(WebElement::isDisplayed).count();
-		return null;
-	}
-
 
 	public void navigatnextpage() {
 		driver.findElement(new By.ByCssSelector("[role=navigation] #pnnext")).click();
 	}
 
-		/*JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("window.scrollBy(0,10000)");
-		//((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", Page2Navigator);
-		try {
-			Thread.sleep(3000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	
-	public void navigatToPage2() {
-		driver.findElement(Page2Navigator).click();
-	}
-	
-	/*public boolean wait() {
-		return wait.until(ExpectedConditions.visibilityOfElementLocated(Page2Navigator)).isDisplayed();
-	}
-	
-	public boolean verifyNewPage() {
-		wait.until(ExpectedConditions.urlToBe("https://www.oculus.com/"));
-		return driver.getTitle().contains("Oculus | VR Headsets & Equipment");
-	}*/
 
 }
--- src/test/java/tests/ClassTest.java
+++ src/test/java/tests/ClassTest.java
@@ -7,38 +7,36 @@ import org.testng.annotations.Test;
 import pages.BasePage;
 import pages.FirstPage;
 import pages.HomePage;
-import pages.SeconedPage;
 
 import java.util.List;
 
 public class ClassTest extends BasePage{
-
-
-
-
-
 	@Test
 	public void TestScenario() throws InterruptedException {
 		HomePage homePage = new HomePage(driver);
+		//// search on google with vodafone
 		homePage.searchProcess("vodafone");
 		FirstPage first = new FirstPage(driver);
 		Thread.sleep(5000);
+		//// count the first page results and save into variable then print the number
 		By searchResultLocator = new By.ByCssSelector("a h3");
 		List<WebElement> elements = driver.findElements(searchResultLocator);
 		int searchResult = (int) elements.stream().filter(WebElement::isDisplayed).count();
 		logger.log(LogStatus.INFO, "First Page count :::"+searchResult);
+		//// navigste to next page
 		first.navigatnextpage();
-		Thread.sleep(3000);
-		SeconedPage seconed = new SeconedPage(driver);
+		//// count the Second page results and save into variable then print the number
+		Thread.sleep(5000);
 		int secondSearchResults = (int) driver.findElements(searchResultLocator).stream().filter(WebElement::isDisplayed).count();
 		logger.log(LogStatus.INFO, "Seconed Page count :::"+secondSearchResults);
-		logger.log(LogStatus.INFO, "Checking if result first page  is equal to seconed page " );
+		//// validate if the two pages result are equal or not
+		logger.log(LogStatus.INFO, "Checking if result first page results  is equal to seconed page results  " );
 		if (searchResult == secondSearchResults){
-			logger.log(LogStatus.INFO,"Passed");
+			logger.log(LogStatus.INFO,"Passed :::: results are equal = " );
 		}
 
 		else if (searchResult != secondSearchResults){
-			logger.log(LogStatus.INFO,"Failed");
+			logger.log(LogStatus.INFO,"Failed :::: results not equal " );
 
 		}
 
